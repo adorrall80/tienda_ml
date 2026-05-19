16e0e843d513d9654167eddbeb35850dc7bd54c4
adorrall80 <adorrall80@gmail.com>
2026-05-19 19:00:38 -0400

    reparacion de bug de test unitarios

--- tienda/tests/TestCase.php
+++ tienda/tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (! Schema::hasTable('roles')) {
+            return;
+        }
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        foreach (['admin', 'vendedor', 'cliente'] as $role) {
+            Role::findOrCreate($role);
+        }
+    }
 }
